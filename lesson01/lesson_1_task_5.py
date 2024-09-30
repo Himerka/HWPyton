6785c4f7f2217eeb4fdef5259d8a7e19332cb3c6
Himerka <nikbamby@mail.ru>
2024-09-30 16:52:54 +0300

    less1

---
+++ lesson01/lesson_1_task_5.py
@@ -0,0 +1,14 @@
+def func_num(num):
+    print(num, end='')
+    #Вызовите эту функцию 11 раз так, чтобы в консоль вывелся номер 88005553535.
+func_num(8)
+func_num(8)
+func_num(0)
+func_num(0)
+func_num(5)
+func_num(5)
+func_num(5)
+func_num(3)
+func_num(5)
+func_num(3)
+func_num(5)
